647b5ccf131d02ed4e7a6a8b9848eaca1f4fbadb
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-09-11 06:03:04 +0800

    New examples added

---
+++ scope01.js
@@ -0,0 +1,33 @@
+// console.log("It works!");
+// const first = "Wes";
+// let second = "Bos";
+// var age = 100;
+//
+// function sayHi() {
+//   console.log("Hi");
+// }
+
+const age = 100;
+
+function go() {
+  const age = 200;
+  const hair = "blonde";
+  console.log(age);
+  console.log(hair);
+}
+
+console.log(age);
+// console.log(hair);
+
+const dog = "snickers";
+
+function logDog() {
+  console.log(dog);
+}
+
+function go() {
+  const dog = "sunny";
+  logDog();
+}
+
+go();
